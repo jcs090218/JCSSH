b46211eb287dd687b1d631ca8ff9be8c6a8d94c2
jenchieh <jcs090218@gmail.com>
2017-12-15 21:43:22 +0800

    add script files.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ========================================================================
+# $File: run.sh $
+# $Date: 2017-12-15 21:42:49 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2017 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+bash ./test/test.sh
