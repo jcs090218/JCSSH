cbb0635c9271b512306ec7d2bbe4d5a050d91bd0
jenchieh <jcs090218@gmail.com>
2017-12-17 17:24:29 +0800

    test compress module.

--- test/test.sh
+++ test/test.sh
@@ -14,13 +14,4 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/../JCSSH.sh"
 
 
-echo "The task is in progress, please wait a few seconds"
-
-
-jcs_progress_bar_5p_arrow 10 Initialize
-jcs_progress_bar_5p_arrow 20 "Phase 1      "
-jcs_progress_bar_5p_arrow 40 "Phase 2      "
-jcs_progress_bar_5p_arrow 60 "Processing..."
-jcs_progress_bar_5p_arrow 80 "Processing..."
-jcs_progress_bar_5p_arrow 90 "Processing..."
-jcs_progress_bar_5p_arrow 100 "Done        "
+jcs_tar_extract "./build/JCSSH.tar.gz" "./build"
